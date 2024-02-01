132d630c01f013078d0cc276cda699aecf2a5375
mxwl888 <158303967+mxwl888@users.noreply.github.com>
2024-02-01 09:47:11 +0800

    注释图片

--- functions/api/bing/wallpaper/index.js
+++ functions/api/bing/wallpaper/index.js
@@ -1,4 +1,4 @@
-export async function onRequest(context) {
+/*export async function onRequest(context) {
     // Contents of context object
     const {
       request, // same as existing Worker API
@@ -17,5 +17,4 @@ export async function onRequest(context) {
     }
     const info = JSON.stringify(return_data);
     return new Response(info);
-
-  }
+  }*/
