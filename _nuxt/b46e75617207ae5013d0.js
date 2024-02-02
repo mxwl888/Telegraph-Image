98f3977e0be0d89219dbfb2ddd1864dbc5b5bdf0
mxwl888 <158303967+mxwl888@users.noreply.github.com>
2024-02-02 09:51:30 +0800

    去除图标

--- _nuxt/b46e75617207ae5013d0.js
+++ _nuxt/b46e75617207ae5013d0.js
@@ -152,8 +152,8 @@
                 staticClass: "header"
             }, [e("svg", {
                 attrs: {
-                    width: "22",
-                    height: "22",
+                    width: "0",
+                    height: "0",
                     viewBox: "0 0 48 48",
                     fill: "none",
                     xmlns: "http://www.w3.org/2000/svg"
